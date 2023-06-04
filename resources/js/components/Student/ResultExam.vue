15dc9b9323862c28b013e8f2b5620d0ff2937636
etienne wayne <etiennewayne@gmail.com>
2023-06-04 08:05:38 +0800

    update some spaces

--- resources/js/components/Student/ResultExam.vue
+++ resources/js/components/Student/ResultExam.vue
@@ -102,8 +102,6 @@
                             </div>
                         </div><!--col--><!--LETTER-->
 
-                        
-
                     </div><!--columns-->
                 </div><!--container-->
             </div>
